a70c8a162acf1ebeb28b1d5158402e7af11c7923
redwheeler3 <jeffo@jeffo.net>
2026-08-27 21:00:39 -0700

    fix(unitListing): update move-in date to November 1, 2026

--- site/_data/site.js
+++ site/_data/site.js
@@ -7,7 +7,7 @@ export default {
   unitListing: {
     type: "3 Bedroom",
     monthlyHousingCharge: "$1,226",
-    moveInDate: "October 1, 2026",
+    moveInDate: "November 1, 2026",
     applicationDeadline: "July 31, 2026",
     householdRequirements: "1 or 2 adults PLUS 2 or more children under 18",
   },
